84d82ddb67ea7cde50b1c0fed48effbf49ee50bd
akanksha <asachdeva@clickup.com>
2023-05-25 21:48:58 +0530

    icons added

--- src/views/admin/chat/index.jsx
+++ src/views/admin/chat/index.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import Icon from "../../../assets/svg/icon.svg";
 import sendIcon from "../../../assets/svg/send.svg";
+import thumbIcon from "../../../assets/svg/thumbsup.svg";
+import messageIcon from "../../../assets/svg/message.svg";
 
 const Chat = () => {
 
@@ -60,34 +62,38 @@ const Chat = () => {
                 message = message.replace(/['"]+/g, '')
                 if (row.turn_actor === "bot") {
                   return (
-              <div className="flex mt-2 space-x-3 max-w-md items-end">
-                <div className="flex-shrink-0 h-[26px] w-[26px] rounded-full flex items-center justify-center">
-                  <img className="w-full" src={Icon} />
-                </div>
-                <div>
-                  <div className="bg-[#F1F1F1] py-4 px-5 rounded-r-[20px] rounded-tl-[20px] w-[290px]">
-                    <span className="font-normal font-TimesNewRoman text-black text-base" dangerouslySetInnerHTML={{ __html: message }}>Hello Mike! How can I assist you today with Clover POS?</span>
-                  </div>
-                </div>
-              </div>
-              )
+                    <div className="flex mt-2 space-x-3 max-w-md items-end">
+                      <div className="flex-shrink-0 h-[26px] w-[26px] rounded-full flex items-center justify-center">
+                        <img className="w-full" src={Icon} />
+                      </div>
+                      <div>
+                        <div className="bg-[#F1F1F1] py-4 px-5 rounded-r-[20px] rounded-tl-[20px] w-[290px] relative">
+                          <span className="font-normal font-TimesNewRoman text-black text-base" dangerouslySetInnerHTML={{ __html: message }}></span>
+                          <div className="absolute right-[20px] gap-[8px] flex">
+                            <img className="h-[26px] w-[26px]" src={thumbIcon} />
+                            <img className="h-[26px] w-[26px]" src={messageIcon} />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
                 }
                 else {
                   return (
-              <div className="flex w-full mt-8 space-x-3 max-w-md ml-auto justify-end">
-                <div>
-                  <div className="bg-green-900 py-4 px-5 text-white p-3 rounded-l-[20px] rounded-tr-[20px] w-[290px]">
-                    <span className="text-base font-normal text-right font-TimesNewRoman"> {message}</span>
-                  </div>
-                </div>
-              </div>
-               )
+                    <div className="flex w-full mt-8 space-x-3 max-w-md ml-auto justify-end">
+                      <div>
+                        <div className="bg-green-900 py-4 px-5 text-white p-3 rounded-l-[20px] rounded-tr-[20px] w-[290px]">
+                          <span className="text-base font-normal text-right font-TimesNewRoman"> {message}</span>
+                        </div>
+                      </div>
+                    </div>
+                  )
                 }
-              })} 
+              })}
               <div className="absolute bg-[#F7F8FA] block w-full h-[76px] right-0 left-0 bottom-0 p-4 flex align-center justify-between gap-[11px]">
                 <input className="placeholder:text-black px-[17px] py-[11px] w-full border border-solid border-[#DFE3E7] rounded-[100px] bg-white font-TimesNewRoman font-normal text-base text-black" placeholder="Type your message here..." />
                 <button className="cursor-pointer">
-                <img src={sendIcon} />
+                  <img src={sendIcon} />
                 </button>
               </div>
             </div>
