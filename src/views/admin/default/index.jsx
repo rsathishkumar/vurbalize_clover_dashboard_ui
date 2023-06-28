e1e9ced4195c873df3687bc006ddf59d7c61ef4c
sathish kumar <sathishkumar@sathishs-Laptop.local>
2023-06-28 19:07:28 +0530

    renamed download file name

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -369,7 +369,7 @@ const Dashboard = () => {
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
         }
-       <CSVLink headers={headers} data={csvData} ref={csvLinkEl} className="absolute invisible">Download</CSVLink>
+       <CSVLink headers={headers} data={csvData} ref={csvLinkEl} filename="conversations.csv" className="absolute invisible">Download</CSVLink>
       </div>
         <div>
           <CheckTable
